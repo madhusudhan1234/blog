90cd7445510a6be4880e8ac6230b5a2f90217ba3
Madhu Sudhan <madhusudhansubedi4@gmail.com>
2018-04-02 23:45:12 +0545

    feat: comment add and delete feature is completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,14 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "madhu", password: "pass", only: :destroy
   def create
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-
+    redirect_to article_path(@article)
+  end
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
     redirect_to article_path(@article)
   end
   private
